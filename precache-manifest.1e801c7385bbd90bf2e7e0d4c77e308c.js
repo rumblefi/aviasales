5addf56a9dffd34225ebb5491acb137c8b41a1bf
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-26 05:20:54 +0200

    Updates

---
+++ precache-manifest.1e801c7385bbd90bf2e7e0d4c77e308c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6431ce84bb7ed628bd308c5066350a2",
+    "url": "/aviasales/static/media/transfer-icon.a6431ce8.svg"
+  },
+  {
+    "revision": "74c86e329d4e4a829cf1162e8bda7163",
+    "url": "/aviasales/static/media/ticketLogo.74c86e32.svg"
+  },
+  {
+    "revision": "331ee049b2a667bedd169a9988bee1ec",
+    "url": "/aviasales/static/media/logo.331ee049.svg"
+  },
+  {
+    "revision": "d1a9076bb7938187cd0fe2359546bebb",
+    "url": "/aviasales/static/media/check-mark.d1a9076b.svg"
+  },
+  {
+    "revision": "907e94a2560f39798db9",
+    "url": "/aviasales/static/js/runtime~main.907e94a2.js"
+  },
+  {
+    "revision": "57c81529c338e59462d6",
+    "url": "/aviasales/static/js/main.57c81529.chunk.js"
+  },
+  {
+    "revision": "88e3046e97d79e968fe8",
+    "url": "/aviasales/static/js/2.88e3046e.chunk.js"
+  },
+  {
+    "revision": "57c81529c338e59462d6",
+    "url": "/aviasales/static/css/main.f2402f0b.chunk.css"
+  },
+  {
+    "revision": "88e3046e97d79e968fe8",
+    "url": "/aviasales/static/css/2.6b5f21f1.chunk.css"
+  },
+  {
+    "revision": "fa8c9f9df98a767166c0336a1be92a3a",
+    "url": "/aviasales/index.html"
+  }
+];
